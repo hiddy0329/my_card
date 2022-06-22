bdf1d2d9866b8fbb22732e81953e64f508c95323
Hidai Satoshi <hiddy19970329@gmail.com>
2022-06-22 21:15:41 +0900

    Containerを追加

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            width: 100.0,
+            height: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
